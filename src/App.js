0b33e2d635cd0eefad2cba6dff5c80145185c8a6
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-07 09:51:46 -0500

    created data.json, appended data list to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { BiArchive } from 'react-icons/bi';
+import { BiArchive, BiTrash } from 'react-icons/bi';
+
 import AddAppointment from './components/AddAppointment';
 import Search from './components/Search';
 
-const App = () => {
+import appointmentList from './data.json'
+
+function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
@@ -10,6 +13,28 @@ const App = () => {
       </h1>
       <AddAppointment />
       <Search />
+
+      <ul className="divide-y divide-gray-200">
+        { appointmentList
+          .map(appointment => {
+            return (
+              <li className="px-3 py-3 flex items-start">
+                <button type="button"
+                  className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <BiTrash /></button>
+                <div className="flex-grow">
+                  <div className="flex items-center">
+                    <span className="flex-none font-medium text-2xl text-blue-500">{ appointment.petName }</span>
+                    <span className="flex-grow text-right">{ appointment.aptDate }</span>
+                  </div>
+                  <div><b className="font-bold text-blue-500">Owner:</b> { appointment.ownerName }</div>
+                  <div className="leading-tight">{ appointment.aptNotes }</div>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
