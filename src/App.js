7665814f45705ea68a045fd79b60a48b43524f6d
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-07 11:48:16 -0500

    built AppointmentInfo component and removed mapped content from App component, renamed AddAppointment from .js to .jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BiArchive, BiTrash } from 'react-icons/bi';
+import { BiArchive } from 'react-icons/bi';
 
-import AddAppointment from './components/AddAppointment';
+import AddAppointment from './components/AddAppointment'
 import Search from './components/Search';
+import AppointmentInfo from './components/AppointmentInfo';
 
 import appointmentList from './data.json'
 
@@ -18,19 +19,7 @@ function App() {
         { appointmentList
           .map(appointment => {
             return (
-              <li className="px-3 py-3 flex items-start">
-                <button type="button"
-                  className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                  <BiTrash /></button>
-                <div className="flex-grow">
-                  <div className="flex items-center">
-                    <span className="flex-none font-medium text-2xl text-blue-500">{ appointment.petName }</span>
-                    <span className="flex-grow text-right">{ appointment.aptDate }</span>
-                  </div>
-                  <div><b className="font-bold text-blue-500">Owner:</b> { appointment.ownerName }</div>
-                  <div className="leading-tight">{ appointment.aptNotes }</div>
-                </div>
-              </li>
+              <AppointmentInfo key={ appointment.id } appointment={ appointment } />
             )
           })
         }
