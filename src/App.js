e6c744a253fde67cbb8de305a75a926378ac9ff2
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-07 16:36:41 -0500

    added sortBy,and orderBy useState in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AppointmentInfo from './components/AppointmentInfo';
 function App() {
   let [ appointmentList, setAppointmentList ] = useState([]);
   let [ query, setQuery ] = useState('');
+  let [ sortBy, setSortBy ] = useState("petName");
+  let [ orderBy, setOrderBy ] = useState("asc")
 
   const filteredAppointments = appointmentList.filter(
     item => {
@@ -18,7 +20,13 @@ function App() {
         item.aptDate.toLowerCase().includes(query.toLowerCase())
       )
     }
-  )
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1
+    return (
+      a[ sortBy ].toLowerCase() < b[ sortBy ].toLowerCase()
+        ? -1 * order : 1 * order
+    )
+  })
 
   const fetchData = useCallback(() => {
     const url = './data.json';
